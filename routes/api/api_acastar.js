94d08e88f940e455ee6c815600b3bc9056133606
김성훈 <seongh7800@gmail.com>
2018-05-10 10:17:40 +0900

    외부과정 연동 추가

--- routes/api/api_acastar.js
+++ routes/api/api_acastar.js
@@ -20,7 +20,38 @@ router.get('/:code/:lectureCode', (req, res, next)=>{
     var lectureCode = req.params.lectureCode
 
     var sql = `
-        SELECT * FROM lecture WHERE lec_idx=? `
+        SELECT
+        	L.lec_idx,
+        	lec_startDate,
+        	lec_endDate,
+        	L.lec_title,
+        	L.lec_personnel,
+        	L.lec_target,
+        	L.lec_time,
+        	L.lec_content,
+        	L.lec_goal,
+        	L.lec_effect,
+        	L.lec_file,
+        	L.lec_flag,
+        	L.lec_sessionCount,
+        	L.lec_serialNo,
+
+        	LS.ls_idx,
+        	LS.ls_title,
+        	LS.ls_location,
+        	LS.ls_seq,
+        	date_format(LS.ls_aplDate, '%Y-%m-%d') as ls_aplDate,
+        	date_format(LS.ls_startDate, '%Y-%m-%d')  as  ls_startDate,
+        	date_format(LS.ls_endDate,   '%Y-%m-%d')  as  ls_endDate
+
+
+        FROM
+        	lecture L
+        	LEFT JOIN lecture_session LS
+        	ON L.lec_idx = LS.lec_idx
+
+        WHERE
+        	L.lec_idx=? `
 
     // 권한검사
     if (code !== 'acastar') {
@@ -28,6 +59,7 @@ router.get('/:code/:lectureCode', (req, res, next)=>{
             status: 403,
             message : '권한없음'
         })
+        return
     }
 
     pool.getConnection((er, connection)=>{
@@ -39,31 +71,51 @@ router.get('/:code/:lectureCode', (req, res, next)=>{
         connection.query(sql, [lectureCode], (err, rs)=>{
             connection.release()
             if (err) {
+                console.log(err);
                 res.send(503, { message: 'query error' })
                 return
             }
 
             // 데이터가 없는경우
             if (rs.length<1) {
-                res.send(204, {
-                    status: 204,
+                res.send(200, {
+                    status: 200,
                     message : 'success - no content'
                 })
                 return
             }
 
+
+
+            var keys = Object.keys(rs)
+            var lecture={
+                "lec_title": rs[0].lec_title,
+                "lec_startDate": rs[0].lec_startDate,
+                "lec_endDate": rs[0].lec_endDate,
+                "lec_flag": rs[0].lec_flag,
+                sessions: []
+            }
+            for(var ii  in  keys){
+                if (rs[ii].ls_idx) {
+                    lecture.sessions.push({
+                        ls_seq: rs[ii].ls_seq+'차',
+                        ls_startDate: rs[ii].ls_startDate,
+                        ls_endDate: rs[ii].ls_endDate,
+                    })
+                }
+            }
+
+
+
             // 데이터가 있는경우
             res.send(200, {
                 status : 200,
                 message: 'success',
-                data: rs
+                lecture
             })
         })
     })
 
-    res.send(200, {
-        response: 'sucess'
-    })
 })
 
 
@@ -89,7 +141,7 @@ router.post('/', (req, res, next)=>{
         }
 
         res.send(200, {
-            status: 403,
+            status: 200,
             message: 'success',
             inputData: lms
         })
